76e01a594ad98ea08f6fcc49736f148c24ddc333
mburgos <miguel.mburgos@gmail.com>
2011-05-24 14:37:37 +0200

    primer commit nueva version

---
+++ func.php
@@ -0,0 +1,115 @@
+<?php
+session_start();
+
+function cabecera()
+{
+ ?>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+ <html>
+  <head>
+  <link rel="stylesheet" type="text/css" media="screen" href="genweb.css" />
+	<meta http-equiv="cache-control" content="no-cache" />
+  <title>GenWeb</title>
+  </head>
+	<body>
+ <?
+}
+
+function conecta()
+{
+ $conn = pg_connect("dbname=genweb user=genweb password=genweb");
+ if(pg_ErrorMessage($conn))
+ {
+  echo "<p><b>Ocurrió un error en la conexión a la base de datos</b></p>";
+	exit;
+ }
+ return $conn;
+}
+
+function desconecta($conn)
+{
+ pg_close($conn);
+}
+
+
+function menu($option)
+{
+?>
+<div class="divmenu">
+<ul class="menu2011">
+<li <?if ($option==1)  print("class=\"verde\"");?>><a href="index.php?option=1">Caracteres</a></li>
+<li <?if ($option==2)  print("class=\"verde\"");?>><a href="index.php?option=2">Proyectos</a></li>
+<li <?if ($option==3)  print("class=\"verde\"");?>><a href="index.php?option=3">Generaciones</a></li>
+</ul>
+</div>
+<?
+}
+
+
+function autentificaform()
+{
+if($_SESSION['ident'])
+{
+?>
+				<h2>Identificado como <?=$_SESSION['user']?>
+<form action="index.php" method="post">
+<input type="submit" value="salir" name="login" />
+</form>
+</h2>
+<?
+}
+else
+{
+?>
+ <h2>Identifícate</h2>
+<form action="index.php" method="post">
+<p>Usuario<input type="text" name="user"></input></p>
+<p>Contrase&ntilde;a<input type="password" name="pass"></input></p>
+<p><input type="submit" value="identificar" name="login" /><input type="reset" value="borrar" /></p>
+<p><input type="submit" value="he olvidado mi contrase&ntilde;a" name="olvido" /></p>
+<p><input type="submit" value="Nuevo Usuario" name="login"/></p>
+</form>
+<?
+}
+?>
+
+<?
+}
+
+function autentifica($user,$pass)
+{
+ $conn = conecta();
+ $sql = "select cod_auth from users where username='$user' and pass='$pass'";
+ echo $sql;
+ $res=pg_query($conn,$sql);
+ $rows=pg_NumRows($res);
+ if ($rows == 1)
+ {
+				 $cod_auth = pg_result($res,0);
+         desconecta($conn);
+				 return $cod_auth;
+ }
+ else
+ {
+	desconecta($conn);
+  return 0;
+ }
+}
+
+
+
+function inserta($user,$pass)
+{
+ $conn = conecta();
+ $sql = "insert into users (cod_auth,username,pass) values (1, '$user','$pass')";
+ echo $sql;
+ $res=pg_query($conn,$sql);
+ desconecta($conn);
+ if($res) return 1;
+ else
+  {
+				 echo "error en la inserción";
+				 return 0;
+  }
+ }
+
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php
+require_once("func.php");
+if(isset($_GET['option'])) $option=$_GET['option']; else $option=0;
+if(isset($_POST['user'])) $user=$_POST['user']; else $user="";
+if(isset($_POST['pass'])) $pass=$_POST['pass']; else $pass="";
+
+cabecera($option);
+
+if($_POST['login'] == 'identificar')
+{
+				if(!isset($_SESSION['ident']))
+				{
+				 $_SESSION['cod_auth'] = autentifica($user,$pass);
+				if ($_SESSION['cod_auth'] > 0)
+				{
+								$_SESSION['ident']=TRUE;
+				}
+				$_SESSION['user']=$user;
+				}
+}
+else if($_POST['login'] == 'Nuevo Usuario')
+{
+				$_SESSION['cod_auth'] = inserta($user,$pass);
+				echo $_SESSION['cod_auth'];
+				if ($_SESSION['cod_auth'] > 0)
+				{
+								$_SESSION['ident']=TRUE;
+				}
+				$_SESSION['user']=$user;
+				}
+
+else if($_POST['login'] == 'salir')
+{
+ session_unset($_SESSION['ident']);
+ $user="";
+ $pass="";
+ $_SESSION['cod_auth']=0;
+}
+
+?>
+
+
+
+
+<h1>Genweb</h1>
+<?//control de usuarios
+autentificaform();
+if($_SESSION['ident'])
+{
+?>
+	<?menu($option);?>
+
+<div class="tab">
+<?
+				switch ($option)
+				{
+         case 1;
+				 echo "opcion 1";
+     break;
+
+         case 2;
+				 echo "opcion 2";
+     break;
+
+         case 3;
+				 echo "opcion 3";
+     break;
+				}
+
+?>
+
+</div>
+
+<?
+}
+?>
+
+</body></html>
