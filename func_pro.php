bdabaef8e814a80f72256c824f5ee9ede6381274
Miguel <mburgos@go.ugr.es>
2021-10-19 12:49:36 +0200

    cambio de directorio de /var/www/proyectosGengine3/ a /var/www/proyectosGengine/

--- func_pro.php
+++ func_pro.php
@@ -90,7 +90,7 @@ function createdir(){
 				$res = pg_query($conn,$sql);
 				$id = pg_fetch_result($res,0,0);
 				pg_close($conn);
-				$path = "/var/www/proyectosGengine3/".$id;
+				$path = "/var/www/proyectosGengine/".$id;
 				mkdir($path);
 }
 
